c63e64a9eaa863f7dab0ee698ec900045d39f074
Nasrisameh <nasri.sameh2016@gmail.com>
2023-11-19 14:17:36 +0100

    fixed some bugs

--- flask/fundamentals/Checkerboard/Checkerboard.py
+++ flask/fundamentals/Checkerboard/Checkerboard.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 # http://localhost:5000 - should display 8 by 8 checkerboard
 @app.route('/')
 def index():
-    return render_template("index.html",row=8,col=8,color1='red',color2='black')
+    return render_template("index.html",row=8,col=8,color1 = 'style = "background-color: red;"',color2 = 'style = "background-color: black;"')
 
 # Your program should have the following output
 # http://localhost:5000/4 - should display 8 by 4 checkerboard
 @app.route('/<int:x>')
 def row(x):
-    return render_template("index.html", row=x, col=8, color1='red', color2='black')
+    return render_template("index.html", row=x, col=8, color1 = 'style = "background-color: red;"', color2 = 'style = "background-color: black;"')
 
 # Your program should have the following output
 # http://localhost:5000/(x)/(y) - should display x by y checkerboard.  
@@ -21,25 +21,25 @@ def row(x):
 # please remember to convert it to integer first (so that you can use x or y in a for loop)
 @app.route('/<int:x>/<int:y>')
 def board(x, y):
-    return render_template('index.html', row=x, col=y, color1='red', color2='black')
+    return render_template('index.html', row=x, col=y, color1 = 'style = "background-color: red;"', color2 = 'style = "background-color: black;"')
 # Have another route accept 2 parameters (i.e. "/<x>/<y>") and render a checkerboard with x rows and y columns, 
 # with alternating colors
 @app.route('/alt/<int:x>/<int:y>')
 def alt_board(x, y):
-    return render_template('index.html', row=x, col=y, color1='blue', color2='green')
+    return render_template('index.html', row=x, col=y, color1 = 'style = "background-color: blue;"', color2 = 'style = "background-color: green;"')
 # Have another route accept 4 parameters (i.e. "/<x>/<y>/<color1>/<color2>") and render a checkerboard with x rows and y columns, 
 # with alternating colors, color1 and color2
 @app.route('/alt-colors/<int:x>/<int:y>')
 def alt_colors_board(x, y):
-    return render_template('index.html', row=x, col=y, color1='pink', color2='white')
+    return render_template('index.html', row=x, col=y, color1 = 'style = "background-color: pink;"', color2 = 'style = "background-color: white;"')
 # change 1er color
 @app.route('/<int:x>/<int:y>/<string:one>')
 def row_col_one(x,y,one):
-    return render_template("index.html",row=x,col=y,color1=one,color2='black')
+    return render_template("index.html",row=x,col=y,color1 = f'style = "background-color: {one};"',color2 = 'style = "background-color: black;"')
 # change 2 colors
 @app.route('/<int:x>/<int:y>/<string:one>/<string:two>')
 def row_col_two(x,y,one,two):
-    return render_template("index.html",row=x,col=y,color1=one,color2=two)
+    return render_template("index.html",row=x,col=y,color1 = f'style = "background-color: {one};"',color2 = f'style = "background-color: {two};"')
 
 if __name__=="__main__":
     app.run(debug=True)
